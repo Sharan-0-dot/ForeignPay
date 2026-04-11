1dd13c3d9628b56a658b48df25bc2cede2860c02
Sharan-0-dot <sharansc482@gmail.com>
2026-04-11 16:05:07 +0530

    Initial setup

---
+++ ForeignPay/src/main/java/com/Payment/ForeignPay/ForeignPayApplication.java
@@ -0,0 +1,13 @@
+package com.Payment.ForeignPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeignPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForeignPayApplication.class, args);
+	}
+
+}
---
+++ ForeignPay/src/test/java/com/Payment/ForeignPay/ForeignPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Payment.ForeignPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
